8217c4214c6d9f436c678661f9ea593f04656a1c
stark world <nicky.kalyan438@gmail.com>
2020-10-22 00:08:20 -0400

    Main Commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { SidenavComponent } from './sidenav/sidenav.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    SidenavComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
